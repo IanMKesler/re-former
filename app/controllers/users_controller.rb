f5f747bf360bf5d39b192aa5c5a01a34e448c9e1
Ian Kesler <ianmkesler@gmail.com>
2019-11-15 22:40:04 -0800

    Add error messages on edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,13 @@ class UsersController < ApplicationController
 
     def update 
         @user = User.find(params[:id])
-        @user.update(user_params)
-        redirect_to edit_user_path
+        @user.assign_attributes(user_params)
+        if @user.save
+            redirect_to edit_user_path
+        else 
+            render :edit
+        end
+        
     end
 
     private_methods
